66e900a7802e64108a7b7def6a585514d931a817
vfa-vupd <vupd@vitalify.asia>
2022-05-17 10:47:50 +0700

    [Update] setup env

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,20 @@
-import Vue from 'vue';
-
+/*import Vue from 'vue';
 import VueRouter from 'vue-router';
-Vue.use(VueRouter);
 
 import VueAxios from 'vue-axios';
 import axios from 'axios';
 import App from './App.vue';
+
+Vue.use(VueRouter);
 Vue.use(VueAxios, axios);
 
 const router = new VueRouter({ mode: 'history'});
+
 new Vue(Vue.util.extend({ router }, App)).$mount('#app');
+*/
+require('./bootstrap');
+import { createApp } from 'vue';
+import homeC from './components/HomeComponent.vue';
+let app=createApp({})
+app.component('home-component' , homeC);
+app.mount("#app")
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -11,7 +11,7 @@
     </head>
     <body>
         <div id="app">
-          <example-component></example-component>
+          <home-component></home-component>
         </div>
         <script src="{{ mix('js/app.js') }}" type="text/javascript"></script>
     </body>
