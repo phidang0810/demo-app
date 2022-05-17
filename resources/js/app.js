e908d1b48b2497e415345596ac6ebd1c8850120f
Diep Phi Dang <phidang@tgm.vn>
2022-05-17 09:51:39 +0700

    init

---
+++ resources/js/app.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+import App from './App.vue';
+Vue.use(VueAxios, axios);
+
+const router = new VueRouter({ mode: 'history'});
+new Vue(Vue.util.extend({ router }, App)).$mount('#app');
